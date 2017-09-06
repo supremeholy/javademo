aae5fcf4526f416a235ae15a41ad0ec414f25782
supremeholy <116670239@qq.com>
2017-09-06 20:59:28 +0800

    add des code

--- src/com/zjdgm/DES.java
+++ src/com/zjdgm/DES.java
@@ -1,7 +1,156 @@
 package com.zjdgm;
 
-/**
- * Created by HL on 2017-09-03.
- */
+import java.security.Key;
+import java.security.SecureRandom;
+import java.security.Security;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+import java.util.Base64;
+
 public class DES {
+
+    public DES(String str) {
+
+    }
+
+    public DES() {
+
+    }
+
+
+    /**
+     * 加密函数
+     *
+     * @param data
+     *            加密数据
+     * @param key
+     *            密钥
+     * @return 返回加密后的数据
+     */
+    public static byte[] encrypt(byte[] data, byte[] key) {
+        try {
+            // DES算法要求有一个可信任的随机数源
+            SecureRandom sr = new SecureRandom();
+
+            // 从原始密钥数据创建DESKeySpec对象
+            DESKeySpec dks = new DESKeySpec(key);
+
+            // 创建一个密匙工厂，然后用它把DESKeySpec转换成
+            // 一个SecretKey对象
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey secretKey = keyFactory.generateSecret(dks);
+
+            // using DES in ECB mode
+            Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+            // 用密匙初始化Cipher对象
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, sr);
+
+            // 执行加密操作
+            byte encryptedData[] = cipher.doFinal(data);
+
+            return encryptedData;
+        } catch (Exception e) {
+            System.err.println("DES算法，加密数据出错!");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * 解密函数
+     *
+     * @param data
+     *            解密数据
+     * @param key
+     *            密钥
+     * @return 返回解密后的数据
+     */
+    public static byte[] decrypt(byte[] data, byte[] key) {
+        try {
+            // DES算法要求有一个可信任的随机数源
+            SecureRandom sr = new SecureRandom();
+
+            // byte rawKeyData[] = /* 用某种方法获取原始密匙数据 */;
+            // 从原始密匙数据创建一个DESKeySpec对象
+            DESKeySpec dks = new DESKeySpec(key);
+
+            // 创建一个密匙工厂，然后用它把DESKeySpec对象转换成
+            // 一个SecretKey对象
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey secretKey = keyFactory.generateSecret(dks);
+
+            // using DES in ECB mode
+            Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+
+            // 用密匙初始化Cipher对象
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, sr);
+
+            // 正式执行解密操作
+            byte decryptedData[] = cipher.doFinal(data);
+
+            return decryptedData;
+        } catch (Exception e) {
+            System.err.println("DES算法，解密出错。");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * 加密String明文输入,String密文输出
+     */
+    public static String getEncString(String strMing,String Secret) {
+        System.out.println("明文:"+strMing);
+        System.out.println("密钥:"+Secret);
+        byte[] byteMi = null;
+        byte[] byteMing = null;
+        String strMi = "";
+        try {
+            byteMing = strMing.getBytes("UTF8");
+            byteMi = encrypt(byteMing, Secret.getBytes());
+            strMi = Base64.getMimeEncoder().encodeToString(byteMi);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Error initializing SqlMap class. Cause: " + e);
+        } finally {
+            byteMing = null;
+            byteMi = null;
+        }
+        System.out.println("加密后密文:"+strMi);
+        return strMi;
+    }
+
+
+    /**
+     * 解密 以String密文输入,String明文输出
+     *
+     * @param strMi
+     * @return
+     */
+    public static String getDesString(String strMi,String Secret) {
+        byte[] byteMing = null;
+        byte[] byteMi = null;
+        String strMing = "";
+        try {
+            byteMi = Base64.getMimeDecoder().decode(strMi);
+            byteMing = decrypt(byteMi, Secret.getBytes() ); //getGameSecret().getBytes());
+            strMing = new String(byteMing, "UTF8");
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Error initializing SqlMap class. Cause: " + e);
+        } finally {
+            byteMing = null;
+            byteMi = null;
+        }
+        return strMing;
+    }
+
 }
